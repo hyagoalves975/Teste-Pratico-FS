592ebdda9aec1ae7098a10f3b9b6f9fb6ed56942
hyago alves <hyagoalves8@gmail.com>
2024-10-27 17:06:28 -0300

    adaptações na entidade, controller e metodo DaoCliente para armazenamento no banco de dados

--- FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
+++ FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
@@ -20,6 +20,7 @@ namespace FI.AtividadeEntrevista.DAL
 
             parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", cliente.Nome));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", cliente.Sobrenome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", cliente.CPF));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Nacionalidade", cliente.Nacionalidade));
             parametros.Add(new System.Data.SqlClient.SqlParameter("CEP", cliente.CEP));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Estado", cliente.Estado));
