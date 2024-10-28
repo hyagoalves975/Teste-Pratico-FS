ac229915cb68c184c59ee25d08355111c43a74d3
hyago alves <hyagoalves8@gmail.com>
2024-10-28 20:11:06 -0300

    Ajustes e adaptações finais para  exibição de dados dinamicos

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -1,33 +1,53 @@
-﻿
+﻿window.beneficiariosList = [];
 $(document).ready(function () {
     if (obj) {
         $('#formCadastro #Nome').val(obj.Nome);
-        $('#formCadastro #CEP').val(obj.CEP);
+        $('#formCadastro #CPF').val(CPFMask(obj.CPF));
+        $('#formCadastro #CEP').val(CepMask(obj.CEP));
         $('#formCadastro #Email').val(obj.Email);
         $('#formCadastro #Sobrenome').val(obj.Sobrenome);
         $('#formCadastro #Nacionalidade').val(obj.Nacionalidade);
         $('#formCadastro #Estado').val(obj.Estado);
         $('#formCadastro #Cidade').val(obj.Cidade);
         $('#formCadastro #Logradouro').val(obj.Logradouro);
-        $('#formCadastro #Telefone').val(obj.Telefone);
+        $('#formCadastro #Telefone').val(PhoneMask(obj.Telefone));
+       
+        if (obj.Beneficiarios && obj.Beneficiarios.length > 0) {
+            obj.Beneficiarios.forEach(function (beneficiario) {
+                addBeneficiarioModal(CPFMask(beneficiario.CPF), beneficiario.Nome);
+            });
+        }
+
     }
 
-    $('#formCadastro').submit(function (e) {
-        e.preventDefault();
+    $('#formCadastro').off('submit').on('submit', function (e) {
+        e.preventDefault(); 
+        if (!cpfValidate($('#CPF').val())) {
+            e.preventDefault();
+            alert(" O CPF digitado é inválido.");
+            $('#CPF').focus();
+            return false;
+        }
+
+        var cpf = clearString($("#CPF").val());
+        var cep = clearString($("#CEP").val());
+        var telefone = clearString($("#Telefone").val());
         
         $.ajax({
             url: urlPost,
             method: "POST",
             data: {
                 "NOME": $(this).find("#Nome").val(),
-                "CEP": $(this).find("#CEP").val(),
+                "CPF": cpf,
+                "CEP": cep,
                 "Email": $(this).find("#Email").val(),
                 "Sobrenome": $(this).find("#Sobrenome").val(),
                 "Nacionalidade": $(this).find("#Nacionalidade").val(),
                 "Estado": $(this).find("#Estado").val(),
                 "Cidade": $(this).find("#Cidade").val(),
                 "Logradouro": $(this).find("#Logradouro").val(),
-                "Telefone": $(this).find("#Telefone").val()
+                "Telefone": telefone,
+                "Beneficiarios": beneficiariosList
             },
             error:
             function (r) {
@@ -44,6 +64,76 @@ $(document).ready(function () {
             }
         });
     })
+
+    $('#CPF').on('input', function () {
+        const cpfInput = $(this).val();
+        $(this).val(CPFMask(cpfInput));
+        clearErrorStyle('#CPF', '#errorMessage')
+
+        if (cpfInput.length > 13) {
+            if (!cpfValidate(cpfInput)) {
+                setErrorStyle('#CPF', '#errorMessage')
+            } else {
+                clearErrorStyle('#CPF', '#errorMessage')
+            }
+        }
+    });
+
+    $('#CPFBeneficiario').on('input', function () {
+        const cpfBeneficiario = $(this).val();
+        $(this).val(CPFMask(cpfBeneficiario));
+        clearErrorStyle('#CPFBeneficiario', '#errorMessageBeneficiarios')
+
+        if (cpfBeneficiario.length > 13) {
+            if (!cpfValidate(cpfBeneficiario)) {
+                setErrorStyle('#CPFBeneficiario', '#errorMessageBeneficiarios')
+            } else {
+                clearErrorStyle('#CPFBeneficiario', '#errorMessageBeneficiarios')
+            }
+        }
+    });
+
+    $('#btnClose').off('click').on('click', function () {
+        clearErrorStyle('#CPFBeneficiario', '#errorMessageBeneficiarios');
+        $('#CPFBeneficiario').val('');
+        $('#NomeBeneficiario').val('');
+    });
+
+    $('#btnIncluirModal').off('click').on('click', function () {
+        let cpfBeneficiario = $('#CPFBeneficiario').val();
+        let nomeBeneficiario = $('#NomeBeneficiario').val();
+
+        if (cpfBeneficiario && nomeBeneficiario) {
+            addBeneficiarioModal(cpfBeneficiario, nomeBeneficiario);
+        } else {
+            alert("Digite o nome e o CPF do Beneficiário")
+        }
+    });
+
+    $(document).off('click', '.btnExcluir').on('click', '.btnExcluir', function () {
+        const cpfBeneficiario = $(this).closest('tr').find('.col-cpfBeneficiario').text();
+        deleteBeneficiarioModal(cpfBeneficiario);
+        $(this).closest('tr').remove();
+    });
+
+    $(document).off('click', '.btnAlterar').on('click', '.btnAlterar', function () {
+        console.log('carregou')
+        clearErrorStyle('#CPFBeneficiario', '#errorMessageBeneficiarios')
+        let row = $(this).closest('tr');
+        let colCPFBeneficiario = row.find('.col-cpfBeneficiario').text();
+        let colNomeBeneficiario = row.find('.col-nomeBeneficiario').text();
+        alterBeneficiarioModal(row, colCPFBeneficiario, colNomeBeneficiario);
+    });
+
+    $('#Telefone').on('input', function () {
+        const phone = $(this).val();
+        $(this).val(PhoneMask(phone));
+    });
+
+    $('#CEP').on('input', function () {
+        const cep = $(this).val();
+        $(this).val(CepMask(cep));
+    });
     
 })
 
---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Utils.js
@@ -0,0 +1,150 @@
+﻿window.beneficiariosList = [];
+
+function CPFMask(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+
+    if (cpf.length <= 3) {
+        return cpf;
+    } else if (cpf.length <= 6) {
+        return cpf.slice(0, 3) + '.' + cpf.slice(3);
+    } else if (cpf.length <= 9) {
+        return cpf.slice(0, 3) + '.' + cpf.slice(3, 6) + '.' + cpf.slice(6);
+    } else {
+        return cpf.slice(0, 3) + '.' + cpf.slice(3, 6) + '.' + cpf.slice(6, 9) + '-' + cpf.slice(9, 11);
+    }
+}
+
+function PhoneMask(phone) {
+    phone = phone.replace(/\D/g, '');
+
+    if (phone.length <= 2) {
+        return phone;
+    } else if (phone.length <= 6) {
+        return '(' + phone.slice(0, 2) + ') ' + phone.slice(2);
+    } else {
+        return '(' + phone.slice(0, 2) + ') ' + phone.slice(2, 6) + '-' + phone.slice(6, 10);
+    }
+}
+
+function CepMask(cep) {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep.length > 5) {
+        cep = cep.slice(0, 5) + "-" + cep.slice(5, 8);
+    }
+
+    return cep;
+}
+
+function cpfValidate(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    let sumDigits = 0;
+    for (let i = 0; i < 9; i++) {
+        sumDigits += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+
+    let firstDigit = (sumDigits * 10) % 11;
+    if (firstDigit === 10 || firstDigit === 11) {
+        firstDigit = 0;
+    }
+
+    if (firstDigit !== parseInt(cpf.charAt(9))) {
+        return false;
+    }
+
+    sumDigits = 0;
+    for (let i = 0; i < 10; i++) {
+        sumDigits += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    let secondDigit = (sumDigits * 10) % 11;
+    if (secondDigit === 10 || secondDigit === 11) {
+        secondDigit = 0;
+    }
+    if (secondDigit !== parseInt(cpf.charAt(10))) {
+        return false;
+    }
+
+    return true;
+}
+
+function setErrorStyle(input, span) {
+    $(input).css('border-color', 'red')
+
+    $(span).show();
+}
+
+function clearErrorStyle(input, span) {
+    $(input).css('border-color', '')
+
+    $(span).hide();
+}
+
+function addBeneficiarioModal(cpfBeneficiario, nomeBeneficiario) {
+    const cpfCliente = $("#CPF").val();
+
+    if (cpfBeneficiario === cpfCliente) {
+        alert('Beneficiário inválido');
+        return false;
+    } else if (!cpfValidate(cpfBeneficiario)) {
+        alert('O CPF digitado é inválido');
+        return false;
+    } else if (validateCPFBeneficiarioModal(cpfBeneficiario)) {
+        alert('Beneficiário já cadastrado');
+        return false;
+    }
+
+    var cpf = clearString(cpfBeneficiario);
+
+    beneficiariosList.push({ CPF: cpf, Nome: nomeBeneficiario });
+
+    var newRowTable =
+        `<tr>
+            <td class="col-cpfBeneficiario">${cpfBeneficiario}</td>
+            <td class="col-nomeBeneficiario">${nomeBeneficiario}</td>
+            <td>
+                <button type="button" class="btn btn-sm btn-primary btnAlterar">Alterar</button>
+                <button type="button" class="btn btn-sm btn-primary btnExcluir">Excluir</button>
+            </td>
+        </tr>`;
+
+    $('#beneficiariosTableBody').append(newRowTable);
+
+    $('#CPFBeneficiario').val('');
+    $('#NomeBeneficiario').val('');
+}
+
+function alterBeneficiarioModal(row, colCPFBeneficiario, colNomeBeneficiario) {
+    $('#CPFBeneficiario').val(colCPFBeneficiario);
+    $('#NomeBeneficiario').val(colNomeBeneficiario);
+
+    deleteBeneficiarioModal(colCPFBeneficiario);
+
+    row.remove();
+}
+
+function deleteBeneficiarioModal(cpfBeneficiario) {
+    var cpfClean = clearString(cpfBeneficiario);
+    window.beneficiariosList = window.beneficiariosList.filter(b => b.CPF !== cpfClean);
+};
+
+function validateCPFBeneficiarioModal(cpfBeneficiario) {
+    let condition = false;
+    $('#beneficiariosTableBody tr').each(function () {
+        const cpfAdicionado = $(this).find('.col-cpfBeneficiario').text();
+
+        if (cpfAdicionado === cpfBeneficiario) {
+            condition = true;
+            return false;
+        }
+    });
+    return condition;
+}
+
+function clearString(input) {
+    return input.replace(/[\.\-\(\) ]/g, '');
+}
